491b069ae2cc77edf5a6cf95d472f20cc1fd0b6f
akara <55230837+richeyphu@users.noreply.github.com>
2024-07-04 23:36:44 +0700

    Update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,66 +1,43 @@
 <template>
-  <nav class="navbar bg-base-100 absolute">
+  <nav class="navbar bg-base-100 absolute z-[1]">
     <div class="navbar-start">
-      <!-- <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
-          </svg>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-        >
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div> -->
       <NuxtLink
         class="btn btn-ghost text-xl drop-shadow-sm font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
         to="/"
         >wallpaper a day</NuxtLink
       >
     </div>
-    <!-- <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div> -->
+    <div class="navbar-center flex"></div>
     <div class="navbar-end">
-      <NuxtLink
-        class="btn btn-outline"
-        to="https://wallpaper-a-day.com/"
-        target="_blank"
-        >Visit original site</NuxtLink
-      >
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost">
+          <Icon name="fe:bar" class="text-2xl" />
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+        >
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/gallery">Gallery</NuxtLink>
+          </li>
+          <div class="divider my-0"></div>
+          <li>
+            <NuxtLink to="https://wallpaper-a-day.com/" target="_blank"
+              >Visit original site</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink
+              to="https://github.com/richeyphu/wallpaper-a-day"
+              target="_blank"
+              >GitHub</NuxtLink
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
