0cbffab83e14c0b420e7b42a809f2e139d37f0d0
akara <55230837+richeyphu@users.noreply.github.com>
2024-07-04 20:37:36 +0700

    Update hero style

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -30,12 +30,12 @@ onMounted(async () => {
     <div class="hero-content text-center">
       <div class="max-w-4xl">
         <h1
-          class="py-1 drop-shadow-sm font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+          class="py-1 drop-shadow-sm font-extrabold text-transparent text-3xl md:text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
         >
           Wallpaper of the Day
         </h1>
         <div
-          class="mt-8 mb-4 [&_img]:rounded-md [&_img]:shadow-xl [&_img]:mb-4 [&_p]:text-sm [&_p]:font-light [&_a]:link-secondary [&_a]:link-hover"
+          class="mt-8 mb-4 [&_img]:rounded-xl [&_img]:shadow-xl [&_img]:mb-4 [&_p]:text-sm [&_p]:font-light [&_a]:link-secondary [&_a]:link-hover"
           v-html="latestPost.content"
           v-if="latestPost"
         ></div>
