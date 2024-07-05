2b41950db00c809a9c5ee909da206ca8aa4bcb90
akara <55230837+richeyphu@users.noreply.github.com>
2024-07-06 01:41:35 +0700

    Add template tag

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -1,3 +1,15 @@
 <script setup lang="ts">
 await navigateTo("/gallery");
 </script>
+
+<template>
+  <div class="hero bg-base-200 min-h-screen py-20">
+    <div class="hero-content text-center">
+      <h1
+        class="py-1 drop-shadow-sm font-bold text-transparent text-3xl md:text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+      >
+        Redirecting...
+      </h1>
+    </div>
+  </div>
+</template>
