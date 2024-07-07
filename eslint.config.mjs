4682785509e157b21f92b02eafc8c12fa389e6a5
akara <55230837+richeyphu@users.noreply.github.com>
2024-07-08 04:55:12 +0700

    chore: Add Prettier configuration and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default withNuxt(
   // Your custom configs here
   {
     rules: {
-      "vue/multi-word-component-names": "off",
-      "vue/no-multiple-template-root": "off",
-    },
+      'vue/multi-word-component-names': 'off',
+      'vue/no-multiple-template-root': 'off'
+    }
   },
   {
-    files: ["nuxt.config.ts"],
+    files: ['nuxt.config.ts'],
     rules: {
-      "no-constant-binary-expression": "off",
+      'no-constant-binary-expression': 'off'
+    }
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin
     },
+    rules: {
+      ...eslintConfigPrettier.rules,
+    }
   }
-);
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-04",
-  srcDir: "./src",
+  compatibilityDate: '2024-07-04',
+  srcDir: './src',
   dir: {
-    public: "../public",
+    public: '../public'
   },
   modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/eslint"
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/eslint'
   ],
   tailwindcss: {
     editorSupport: true,
-    configPath: "./tailwind.config.ts",
+    configPath: './tailwind.config.ts'
   },
   googleFonts: {
     families: {
-      "Noto Sans Thai": "100..900",
-    },
+      'Noto Sans Thai': '100..900'
+    }
   },
   runtimeConfig: {
     public: {
       baseUrl:
-        "" ||
-        (process.env.NODE_ENV === "production"
-          ? "https://wallpaper-a-day.vercel.app"
-          : "http://localhost:3000"),
-      countApiUrl: "",
-    },
-  },
-});
+        '' ||
+        (process.env.NODE_ENV === 'production'
+          ? 'https://wallpaper-a-day.vercel.app'
+          : 'http://localhost:3000'),
+      countApiUrl: ''
+    }
+  }
+})
